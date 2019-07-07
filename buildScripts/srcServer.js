6732fb9df1128ec641ecdf98d90e83ee37929041
Eric Magesho <ericmagesho@gmail.com>
2019-07-07 20:25:34 +0300

    Added babel for transpiling

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const path = require("path");
-const open = require("open");
+import express from 'express';
+import  path from 'path'
+import open from 'open';
 
 const port = 3000;
 
-var app = express();
+const app = express();
 
 
 app.get('/', (req,res)=> {
