33ec3c13550be70eab55bc8d008136b3a96b2e62
emagesho <ericmagesho@gmail.com>
2019-07-08 17:02:46 +0300

    added eslint and eslint-watch

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import './index.css';
 import numeral from 'numeral';
 
 const courseValue = numeral(1000).format('Ksh0,0.00');
-console.log(`I wuold pay ${courseValue} for this awesome course`);
+console.log(`I wuold pay ${courseValue} for this awesome course`);//eslint-disable-line no-console
